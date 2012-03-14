fbe540df9ebbe1b798567e3574b558523022a437
Marcus McDuffie <whitehatpsycho@gmail.com>
2012-03-14 16:09:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarcusBlog::Application.config.secret_token = 'c2a87f4957acefb17fced7d850392e33b0c6ba9d558ef08fab1aaee3f245d77e6469b6428415291aa39a0929d2d7a26d1278fa6cc03303233e2f1a214e837878'
