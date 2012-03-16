40afc4b56c1157db188098b5cb66f50bdb4e2684
Marcus McDuffie <whitehatpsycho@gmail.com>
2012-03-16 10:49:31 -0400

    added basic comment support

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120315185948) do
+ActiveRecord::Schema.define(:version => 20120316122014) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "author"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "post_id"
+  end
 
   create_table "posts", :force => true do |t|
     t.datetime "created_at", :null => false
