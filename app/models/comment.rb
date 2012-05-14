a9df1593a66baa18b5d2d736f291a2272dfc2f75
Marcus McDuffie <whitehatpsycho@gmail.com>
2012-05-14 14:18:31 -0400

    Now it might actualy block spam.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   include Rakismet::Model
+  before_create :check_for_spam
   belongs_to :post
   validates_format_of :author_url, :with => URI::regexp(%w(http https)), :allow_blank => true
+  def check_for_spam
+    if self.spam? == true
+      return false
+    end
+  end
 end
